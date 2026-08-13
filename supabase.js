85a34dcaa7a77c727cb2ea8eab61973d35eebe3b
johnf02 <80365005+johnf02@users.noreply.github.com>
2026-08-13 17:54:16 -0500

    Update supabase.js datos supabase

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://TU-PROJECT.supabase.co',
-  'TU-API-KEY'
+  'https://unlvsbwpottsnphxudsr.supabase.co/rest/v1/',
+  'sb_publishable_bxaHosrwORoJkXdZypBgqQ_O0C6DAe9'
 )
