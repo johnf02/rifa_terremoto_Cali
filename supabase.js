4c6c7993f37cc418b8b13063ee129a60f56b36ea
johnf02 <80365005+johnf02@users.noreply.github.com>
2026-08-13 19:19:37 -0500

    Update supabase.js v2

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
-const SUPABASE_URL = "https://unlvsbwpottsnphxudsr.supabase.co/rest/v1/"
+const SUPABASE_URL = "https://unlvsbwpottsnphxudsr.supabase.co/"
 const SUPABASE_KEY = "sb_publishable_bxaHosrwORoJkXdZypBgqQ_O0C6DAe9"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
