71cab414ed23148e3937501462f3a1c3b98f37a7
johnf02 <80365005+johnf02@users.noreply.github.com>
2026-08-13 19:01:51 -0500

    Update supabase.js cambio final

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
 // ⚠️ Usa la anon key (clave pública) de Supabase, nunca la service_role
-const SUPABASE_URL = "https://TU-PROJECT.supabase.co"
-const SUPABASE_KEY = "TU-ANON-KEY"
+const SUPABASE_URL = "https://unlvsbwpottsnphxudsr.supabase.co/rest/v1/"
+const SUPABASE_KEY = "sb_publishable_bxaHosrwORoJkXdZypBgqQ_O0C6DAe9"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
